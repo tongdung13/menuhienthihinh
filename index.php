2d32f73581ec98b2d3c451453670255ed04869ba
tongdung13 <tongvanthienvu@gmail.com>
2020-11-07 08:44:56 +0700

    ok

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; chatset=utf-8"/>
+    <title>Hiển thị các hình</title>
+</head>
+<body>
+        <nav>
+            <a href="rectangle.php">Rectangle</a><br><br>
+            <a href="squaretriangle.php">Square Triangle</a><br><br>
+            <a href="isoscelestriangle.php">Isosceles Triangle</a>
+        </nav>
+
+</body>
+</html>
